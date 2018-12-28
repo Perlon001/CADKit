6657648e5e6a9fcfbab19317dad09a60337490dc
perlon <perlon@op.pl>
2018-12-28 23:19:02 +0100

    20181228

---
+++ CADKit.Database/Contract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADKitDALDB.Contract
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter = null, IList<Expression<Func<T, object>>> includedProperties = null, IList<ISortCriteria<T>> sortCriterias = null);
+        PaginatedList<T> GetPaged(Expression<Func<T, bool>> filter = null, IList<Expression<Func<T, object>>> includedProperties = null, PagingOptions<T> pagingOptions = null);
+        T Find(Expression<Func<T, bool>> filter, IList<Expression<Func<T, object>>> includedProperties = null);
+        void Add(T t);
+        void Remove(T t);
+        void Remove(Expression<Func<T, bool>> filter);
+    }
+}
